9dc4e0648b72ddce443219d1fb9b88b0bc540823
Joel Shapiro <joel.shapiro@divido.com>
2021-08-18 13:28:01 +0200

    feat(single-tenancy): Observer for checking config changes

---
+++ Observer/ConfigChangeObserver.php
@@ -0,0 +1,44 @@
+<?php
+namespace Divido\DividoFinancing\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Message\ManagerInterface;
+
+class ConfigChangeObserver implements ObserverInterface
+{
+    public const CONFIG_XPATH_ENVIRONMENT_URL = 'payment/divido_financing/environment_url';
+    public const CONFIG_XPATH_API_KEY = 'payment/divido_financing/api_key';
+
+    public $dataHelper;
+    private $request;
+    private $messageManager;
+
+    public function __construct(
+        \Divido\DividoFinancing\Helper\Data $dataHelper,
+        ManagerInterface $messageManager
+    ) {
+        $this->messageManager = $messageManager;
+        $this->dataHelper = $dataHelper;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $eventData = $observer->getEvent()->getData();
+
+        if(!array_key_exists('changed_paths', $eventData)){
+            return;
+        }
+
+        $changedPaths = $eventData['changed_paths'];
+        // Check that the values we are interested in
+        if(
+            !in_array(self::CONFIG_XPATH_ENVIRONMENT_URL, $changedPaths) &&
+            !in_array(self::CONFIG_XPATH_ENVIRONMENT_URL, $changedPaths)
+        ){
+            //
+            return;
+        }
+
+        $this->messageManager->addErrorMessage('Noep, no dice.');
+    }
+}
