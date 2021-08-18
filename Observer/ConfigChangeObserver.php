015e49aef95ad79aafc22bc73a8f95cdec081f13
Joel Shapiro <joel.shapiro@divido.com>
2021-08-18 14:30:47 +0200

    feat(tests): Testing that we only call if one of the properties we are interested in changes

--- Observer/ConfigChangeObserver.php
+++ Observer/ConfigChangeObserver.php
@@ -10,7 +10,6 @@ class ConfigChangeObserver implements ObserverInterface
     public const CONFIG_XPATH_API_KEY = 'payment/divido_financing/api_key';
 
     public $dataHelper;
-    private $request;
     private $messageManager;
 
     public function __construct(
@@ -33,11 +32,12 @@ class ConfigChangeObserver implements ObserverInterface
         // Check that the values we are interested in
         if(
             !in_array(self::CONFIG_XPATH_ENVIRONMENT_URL, $changedPaths) &&
-            !in_array(self::CONFIG_XPATH_ENVIRONMENT_URL, $changedPaths)
+            !in_array(self::CONFIG_XPATH_API_KEY, $changedPaths)
         ){
-            //
             return;
         }
+
+        $this->dataHelper->getApiKey();
 
         $this->messageManager->addErrorMessage('Noep, no dice.');
     }
