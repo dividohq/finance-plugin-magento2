7fdf5b18cb4006b733bc0b5dc6012d27ba531114
Andrew Smith <andrew.smith@divido.com>
2021-12-02 12:14:37 +0000

    chore: namespace logging

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -3,4 +3,29 @@ namespace Divido\DividoFinancing\Logger;
 
 class Logger extends \Monolog\Logger
 {
+    const NAMESPACE = "PoweredByDivido";
+    
+    public function info(string $message){
+
+        $newMessage = self::NAMESPACE.": {$message}";
+        return parent::info($newMessage);
+    }
+
+    public function error(string $message){
+
+        $newMessage = self::NAMESPACE.": {$message}";
+        return parent::error($newMessage);
+    }
+
+    public function debug(string $message){
+
+        $newMessage = self::NAMESPACE.": {$message}";
+        return parent::debug($newMessage);
+    }
+
+    public function warning(string $message){
+
+        $newMessage = self::NAMESPACE.": {$message}";
+        return parent::warning($newMessage);
+    }
 }
