c9ed9938685d2dccca0d757009f85ff9b72e7f5a
Andrew Smith <andrew.smith@divido.com>
2022-11-15 17:35:45 +0000

    chore: tidy that up a bit

--- Controller/Financing/Success.php
+++ Controller/Financing/Success.php
@@ -44,14 +44,15 @@ class Success extends \Magento\Framework\App\Action\Action implements CsrfAwareA
 
     public function getTimeout()
     {
-        $timeout = $this->config->getValue(
+        $timeout = (int) $this->config->getValue(
             'payment/divido_financing/timeout_delay',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
-        if($timeout < self::MIN_TIMEOUT){
-            self::MIN_TIMEOUT;
-        }
-        return $timeout;
+        
+        return ($timeout < self::MIN_TIMEOUT)
+            ? self::MIN_TIMEOUT
+            : $timeout;
+
     }
 
     public function execute()
