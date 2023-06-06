27c61d075a1cac3cb81f5d97f5a2d24541741750
Andivido <andrew.smith@divido.com>
2023-06-06 19:55:39 +0100

    feat: adds an info block to display info to the merchant prior to refund attempt

---
+++ Block/Adminhtml/Order/Creditmemo/Create/Custom.php
@@ -0,0 +1,57 @@
+<?php
+namespace Divido\DividoFinancing\Block\Adminhtml\Order\Creditmemo\Create;
+
+class Custom extends \Magento\Backend\Block\Template
+{
+    private $helper;
+    private $_coreRegistry;
+
+    const REFUND_REASONS = [
+        "ALTERNATIVE_PAYMENT_METHOD_USED" => "Alternative Payment Method Used",
+        "GOODS_FAULTY" => "Goods Faulty",
+        "GOODS_NOT_RECEIVED" => "Goods Not Received",
+        "GOODS_RETURNED" => "Goods Returned",
+        "LOAN_AMENDED" => "Loan Amended",
+        "NOT_GOING_AHEAD" => "Not Going Ahead",
+        "NO_CUSTOMER_INFORMATION" => "No Customer Information"
+    ];
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $_coreRegistry,
+        \Divido\DividoFinancing\Helper\Data $helper,
+        array $data = []
+    ) {
+        $this->helper = $helper;
+        $this->_coreRegistry = $_coreRegistry;
+        parent::__construct($context, $data);
+    }
+
+    public function getCreditMemo() {
+        $creditMemo = $this->_coreRegistry->registry('current_creditmemo');
+        return $creditMemo;
+    }
+
+    public function getApplication(){
+        $creditMemo = $this->getCreditMemo();
+        $order = $creditMemo->getOrder();
+        
+        $code  = $order->getPayment()->getMethodInstance()->getCode();
+        $application = [];
+        $autoRefund = $this->config->getValue(
+            'payment/divido_financing/auto_refund',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        if ($code == 'divido_financing' && !empty($this->helper->getApiKey() && $autoRefund) {
+            $application['refundable']['int'] = $this->helper->getRefundableAmount($order);
+            $application['refundable']['float'] = ($application['refundable']['int']/100);
+            if($this->helper->getPlatformEnv() === 'novuna'){
+                $application['reasons'] = self::REFUND_REASONS;
+            }
+        }
+        
+        return $application;
+    }
+
+
+}
