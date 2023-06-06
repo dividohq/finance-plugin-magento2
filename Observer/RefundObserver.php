0f820ed4d327956aeec946d45e1d59a5e19f8bf1
Andivido <andrew.smith@divido.com>
2023-06-06 19:56:20 +0100

    feat: tweaks RefundObserver to obtain info from new block

--- Observer/RefundObserver.php
+++ Observer/RefundObserver.php
@@ -6,19 +6,31 @@ use Magento\Framework\Event\ObserverInterface;
 class RefundObserver implements ObserverInterface
 {
     public $helper;
+
+    public $_request;
+
+    public $logger;
+
     public function __construct(
-        \Divido\DividoFinancing\Helper\Data $helper
+        \Magento\Framework\App\RequestInterface $request,
+        \Divido\DividoFinancing\Helper\Data $helper,
+        \Divido\DividoFinancing\Logger\Logger $logger
     ) {
-    
+        $this->_request = $request;
         $this->helper = $helper;
+        $this->logger = $logger;
     }
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $order = $observer->getEvent()->getCreditmemo()->getOrder();
-        
         $code  = $order->getPayment()->getMethodInstance()->getCode();
-        if ($code == 'divido_financing') {
-            return $this->helper->autoRefund($order);
+        $params = $this->_request->getParams();
+            
+        if ($code == 'divido_financing' && isset($params['pbd_refund']) && $params['pbd_refund'] > 0) {
+            $this->logger->info('PBD Refund Triggered', ['Quote' => $order->getQuoteId(), 'request' => $params]);
+
+            $reason = $params['refund_reason'] ?? null;
+            $this->helper->autoRefund($order, $params['pbd_refund'], $reason);
         }
     }
 }
