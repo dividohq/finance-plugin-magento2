ee43cadacece25d60a8343c622a955a9cc4b4805
Andivido <andrew.smith@divido.com>
2023-06-08 15:09:59 +0100

    chore: catch MerchantApiBadeResponseException

--- Block/Adminhtml/Order/Creditmemo/Create/Custom.php
+++ Block/Adminhtml/Order/Creditmemo/Create/Custom.php
@@ -39,12 +39,22 @@ class Custom extends \Magento\Backend\Block\Template
         $code  = $order->getPayment()->getMethodInstance()->getCode();
         $application = [];
         $autoRefund = $this->helper->getAutoRefund();
-        
+
         if ($code == 'divido_financing' && !empty($this->helper->getApiKey()) && $autoRefund) {
-            $application['refundable']['int'] = $this->helper->getRefundableAmount($order);
-            $application['refundable']['float'] = ($application['refundable']['int']/100);
-            if($this->helper->getPlatformEnv() === 'novuna'){
-                $application['reasons'] = self::REFUND_REASONS;
+            try{
+                $application['refundable']['int'] = $this->helper->getRefundableAmount($order);
+                $application['refundable']['float'] = ($application['refundable']['int']/100);
+                $application['notification'] = sprintf(
+                    "Please note you can only be refunded &pound;%s currently. You must contact your lender in order to obtain a refund for any deposit, etc.", 
+                    $application['refundable']['float']
+                );
+
+                if($this->helper->getPlatformEnv() === 'novuna'){
+                    $application['reasons'] = self::REFUND_REASONS;
+                }
+            } catch (\Divido\MerchantSDK\Exceptions\MerchantApiBadResponseException $_){
+                $application['notification'] = "It appears you are using a different API Key to the one used to create this application.&nbsp;
+                Please revert to that API key if you wish to automatically request this amount is refunded.";
             }
         }
         
