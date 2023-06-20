776cec02e82930f44401c3b4a6bf71d7bb2e1d14
Andivido <andrew.smith@divido.com>
2023-06-20 12:29:45 +0100

    chore: improve wording

--- Block/Adminhtml/Order/Creditmemo/Create/Custom.php
+++ Block/Adminhtml/Order/Creditmemo/Create/Custom.php
@@ -41,20 +41,23 @@ class Custom extends \Magento\Backend\Block\Template
             try{
                 $application = $this->helper->getApplicationFromOrder($order);
                 $returnApp['refundable'] = $application['amounts']['refundable_amount'];
-                $returnApp['notifications'][] = sprintf(__("The maximum refund available for this application is %s"),  $order->formatPrice($returnApp['refundable']/100));
+                $returnApp['notifications'][] = sprintf(
+                    __("The de-facto amount refundable for this application is %s. Any refund attempt exceding this will be processed as a full refund for %s"),
+                    $order->formatPrice($returnApp['refundable']/100),
+                    $order->formatPrice($returnApp['refundable']/100)
+                );
+
                 if(in_array($application['lender']['app_name'], DATA::NON_PARTIAL_LENDERS)){
                     $returnApp['notifications'][] = __("We are unable to request partial refunds from your lender");
                     $returnApp['partial_refundable'] = 0;
-                }else{
-                    if($application['finance_plan']['credit_amount']['minimum_amount'] > 0){
-                        $returnApp['partial_refundable'] = $application['amounts']['refundable_amount'] - $application['finance_plan']['credit_amount']['minimum_amount'];
-                        $returnApp['notifications'][] = sprintf(
-                            __("If you are making a partial refund, the maximum that can be refunded (before reaching this finance plan's minimum credit limit) is %s"), 
-                            $order->formatPrice($returnApp['partial_refundable']/100)
-                        );
-                    }
+                }elseif($application['finance_plan']['credit_amount']['minimum_amount'] > 0){
+                    $returnApp['partial_refundable'] = $application['amounts']['refundable_amount'] - $application['finance_plan']['credit_amount']['minimum_amount'];
+                    $returnApp['notifications'][] = sprintf(
+                        __("If you are making a partial refund, the maximum that can be refunded (before reaching this finance plan's minimum credit limit) is %s"), 
+                        $order->formatPrice($returnApp['partial_refundable']/100)
+                    );
                 }
-
+               
                 $returnApp['notifications'][] = __("Please turn off automatic refunds in the Powered By Divido plugin if you wish to create refunds outside of the above scope");
 
                 if(array_key_exists($application['lender']['app_name'], Data::REFUND_CANCEL_REASONS)){
