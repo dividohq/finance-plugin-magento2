72c38468897418736263376de9b2606623b889bd
Andrew Smith <42767996+andivido@users.noreply.github.com>
2023-11-16 17:39:39 +0000

    chore: adds some new Exceptions for Webhook flow

---
+++ Exceptions/OrderNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Divido\DividoFinancing\Exceptions;
+
+use Exception;
+
+class OrderNotFoundException extends Exception{
+
+}
---
+++ Exceptions/UnusedWebhookException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Divido\DividoFinancing\Exceptions;
+
+use Exception;
+
+class UnusedWebhookException extends Exception{
+
+}
